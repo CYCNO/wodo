6221160526f2f3135f95f3057250c49a7162f2f7
CYCNO <daniyalchunna2@gmail.com>
2025-10-03 12:08:17 +0530

    fixed some typos

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 // =============================================
 // Author: CYCNO
 // For more info about code see contribution.md
-// Team Errorly
 // =============================================
 
 use std::env::args;
